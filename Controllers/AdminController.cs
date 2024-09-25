95d7087d480a0d13f4f3fd32e4a333f168a25c51
samadhan7 <samadhanjejurkar78@gmail.com>
2024-09-25 13:21:50 +0530

    initial commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using GTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GTL.Controllers
+{
+	public class AdminController : Controller
+	{
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+		public IActionResult Openings()
+		{
+			return View();
+		}
+
+		public IActionResult Applications()
+		{
+			return View();
+		}
+
+		public IActionResult Inquiries()
+		{
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
